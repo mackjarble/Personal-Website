25924cb1da564ad358da67bb57269d659930d29e
mackjarble <mackjarble@gmail.com>
2023-04-10 13:01:25 -0400

    Security updates and email mods

--- php/mail.php
+++ php/mail.php
@@ -1,10 +1,25 @@
 <?php
+session_start();
+
 // Check if the form was submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  // Validate the CSRF token
+  if ($_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+    echo "Invalid CSRF token";
+    exit();
+  }
+
   // Get the form data
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
+  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+  $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+
+  // Validate the email address
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Invalid email address";
+    exit();
+  }
 
   // Set the recipient email address
   $to = 'mackjarble@gmail.com';
@@ -18,7 +33,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $body .= 'Message: ' . $message . "\n";
 
   // Send the email
-  $result = mail($to, $subject, $body);
+  $headers = "From: $email";
+  $result = mail($to, $subject, $body, $headers);
 
   if ($result) {
     echo "Email sent successfully";
